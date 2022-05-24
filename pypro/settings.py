be191d9fb3e2db65988da7ab9a3822a8aa8680da
Letícia <leticiaalaura@gmail.com>
2022-05-24 16:08:37 -0300

    Lib Python Decouple

--- pypro/settings.py
+++ pypro/settings.py
@@ -17,7 +17,6 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
