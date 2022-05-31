8108bbbb7c35b6d2b30053b8461ed708b9a88e43
Letícia <leticiaalaura@gmail.com>
2022-05-31 09:38:45 -0300

    Configuradas variáveis do AWS

--- pypro/settings.py
+++ pypro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
 ]
 
@@ -127,6 +128,8 @@ STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLE = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 #STORAGE CONFIGURATION IN S3 AWS
@@ -140,6 +143,9 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
+
+    COLLECTFAST_ENABLE = True
+
     AWS_DEFAULT_ACL = 'private'
 
     # Static Assets
