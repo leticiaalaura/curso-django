c5a1d4ff0aceda9f94b2a27e891fa9d2d73bafcc
Letícia <leticiaalaura@gmail.com>
2022-06-02 18:45:09 -0300

    Comando Makemigrations criado

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -9,21 +9,21 @@ from django.utils.translation import gettext_lazy as _
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
         """
-        if not username:
+        if not email:
             raise ValueError("The given username must be set")
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.password = make_password(password)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
@@ -78,7 +78,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         Return the first_name plus the last_name, with a space in between.
         """
-        full_name = "%s %s" % (self.first_name)
+        full_name = "%s" % (self.first_name)
         return full_name.strip()
 
     def get_short_name(self):
