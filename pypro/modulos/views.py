3a1b9c717a6cf68d8b5230c531523ffa849987bb
Letícia <leticiaalaura@gmail.com>
2022-07-26 15:26:24 -0300

    Implementando indice de módulo

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from pypro.modulos import fachada
 
 
+def indice(request):
+    ctx = {'modulos': fachada.listar_modulos_com_aulas()}
+    return render(request, 'modulos/indice.html', ctx)
+
+
 def detalhe(request, slug):
     modulo = fachada.encontrar_modulo(slug)
     aulas = fachada.listar_aulas_de_modulo_ordenadas(modulo)
