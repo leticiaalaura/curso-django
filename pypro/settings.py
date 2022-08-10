23a2d94acfec5fcf3a59510605d5f34bc49b6fac
Letícia <leticiaalaura@gmail.com>
2022-08-10 13:31:39 -0300

    Configuração parâaetros de envio de email

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,6 +42,10 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'pypro.base',
+    'pypro.turmas',
+    'pypro.aperitivos',
+    'pypro.modulos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,10 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'collectfast',
     'django.contrib.staticfiles',
-    'pypro.base',
-    'pypro.turmas',
-    'pypro.aperitivos',
-    'pypro.modulos',
     'ordered_model',
     'django_extensions',
 ]
@@ -88,10 +88,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
+# Configuração de envio de e-mail
+
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 # Configuração Django Debug Toolbar
 
-INTERNAL_IPS=config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
 
 if DEBUG:
     INSTALLED_APPS.append('debug_toolbar')
